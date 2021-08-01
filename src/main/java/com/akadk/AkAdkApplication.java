36b51d26f19edabaf7e21f6b510c3fcac79912cc
DiMaSproGram <averasok0808@gmail.com>
2021-08-02 01:00:05 +0300

    first commit

---
+++ src/main/java/com/akadk/AkAdkApplication.java
@@ -0,0 +1,11 @@
+package com.akadk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkAdkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AkAdkApplication.class, args);
+    }
+}
